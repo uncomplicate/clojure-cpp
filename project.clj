68dddb5c0b2208b71e791ee58c1b6a08459023d8
Dragan Djuric <dragandj@gmail.com>
2025-09-09 00:08:24 +0200

    Fix version string.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject org.uncomplicate/clojure-cpp "0.6.0."
+(defproject org.uncomplicate/clojure-cpp "0.6.0"
   :description "Clojure native interop through JavaCPP"
   :author "Dragan Djuric"
   :url "http://github.com/uncomplicate/clojure-cpp"
